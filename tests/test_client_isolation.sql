3463d0a5718d923cccfe983fdd416daab09a99ef
Amir Zarifbekov <114651218+Amirchik-1@users.noreply.github.com>
2026-04-19 09:05:41 +0300

    Update test_client_isolation.sql

--- tests/test_client_isolation.sql
+++ tests/test_client_isolation.sql
@@ -1,1 +1,10 @@
+-- Этот тест должен выполняться от имени client_ivan
+-- Ожидается: только 2 аренды (Ивана)
 
+\echo '=== Тест: клиент видит только свои аренды ==='
+SELECT rental_id, equipment_id, total_price, status 
+FROM rentals 
+WHERE client_id = 1;
+
+\echo '=== Попытка увидеть чужую аренду (должно быть 0 строк) ==='
+SELECT * FROM rentals WHERE client_id = 2;
