87e552d0b5acdae36003cffb41988b4e8c3902fb
Amir Zarifbekov <114651218+Amirchik-1@users.noreply.github.com>
2026-04-19 09:03:09 +0300

    Update 02_roles.sql

--- sql/02_roles.sql
+++ sql/02_roles.sql
@@ -1,1 +1,25 @@
+-- Роли пользователей системы
+DROP ROLE IF EXISTS r_client;
+DROP ROLE IF EXISTS r_manager;
+DROP ROLE IF EXISTS r_admin;
 
+CREATE ROLE r_client;
+CREATE ROLE r_manager;
+CREATE ROLE r_admin;
+
+-- Создаём конкретных пользователей
+DROP USER IF EXISTS client_ivan;
+DROP USER IF EXISTS client_petr;
+DROP USER IF EXISTS manager_maria;
+DROP USER IF EXISTS manager_dmitry;
+
+CREATE USER client_ivan WITH PASSWORD 'ivan123';
+CREATE USER client_petr WITH PASSWORD 'petr123';
+CREATE USER manager_maria WITH PASSWORD 'maria123';
+CREATE USER manager_dmitry WITH PASSWORD 'dmitry123';
+
+-- Назначаем роли пользователям
+GRANT r_client TO client_ivan;
+GRANT r_client TO client_petr;
+GRANT r_manager TO manager_maria;
+GRANT r_manager TO manager_dmitry;
