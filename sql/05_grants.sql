5a1fbb1a35b32d70b7998b31d9d5c5c17da8aaab
Amir Zarifbekov <114651218+Amirchik-1@users.noreply.github.com>
2026-04-19 09:04:14 +0300

    Update 05_grants.sql

--- sql/05_grants.sql
+++ sql/05_grants.sql
@@ -1,1 +1,11 @@
+-- Права для клиентов
+GRANT SELECT ON rentals TO r_client;
+GRANT SELECT ON equipment TO r_client;
 
+-- Права для менеджеров
+GRANT SELECT, INSERT, UPDATE ON rentals TO r_manager;
+GRANT SELECT ON equipment TO r_manager;
+GRANT SELECT (full_name, phone) ON clients TO r_manager;
+
+-- Права для администратора
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO r_admin;
